029ff898e94d44e351bc2f90a383754dd1b5b1c7
LinX <44514881+L261173157@users.noreply.github.com>
2021-08-22 23:26:02 +0800

    增加不同region块引用

---
+++ Component/ModuleName.cs
@@ -0,0 +1,21 @@
+﻿using Component.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+
+namespace Component
+{
+    public class ModuleName : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
